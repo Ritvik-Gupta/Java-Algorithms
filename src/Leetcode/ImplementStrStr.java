c09caa1b606de82e27a6aa86741a0ed7051f4abd
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2022-03-15 22:23:29 +0530

    Solved strstr() Leetcode problem using Hashing algortihms

--- src/Leetcode/ImplementStrStr.java
+++ src/Leetcode/ImplementStrStr.java
@@ -4,22 +4,36 @@ public class ImplementStrStr {
 }
 
 class Solution {
+    private static int HASH_BASE = 1000000;
+    private static int HASH_OFFSET = 31;
+
     public int strStr(String haystack, String needle) {
         if (needle.length() == 0)
             return 0;
 
-        for (int i = 0; i < haystack.length(); ++i) {
-            if (haystack.charAt(i) == needle.charAt(0)) {
-                boolean found = true;
-                for (int j = 1; j < needle.length(); ++j) {
-                    if (i + j >= haystack.length() || haystack.charAt(i + j) != needle.charAt(j)) {
-                        found = false;
-                        break;
-                    }
-                }
-                if (found)
-                    return i;
+        int needleLen = needle.length();
+
+        int power = 1, needleCode = 0;
+
+        for (int i = 0; i < needleLen; i++) {
+            power = (power * HASH_OFFSET) % HASH_BASE;
+            needleCode = (needleCode * HASH_OFFSET + needle.charAt(i)) % HASH_BASE;
+        }
+
+        int hashCode = 0;
+        for (int i = 0; i < haystack.length(); i++) {
+            hashCode = (hashCode * HASH_OFFSET + haystack.charAt(i)) % HASH_BASE;
+            if (i < needleLen - 1)
+                continue;
+
+            if (i >= needleLen) {
+                hashCode = (hashCode - haystack.charAt(i - needleLen) * power) % HASH_BASE;
+                if (hashCode < 0)
+                    hashCode += HASH_BASE;
             }
+
+            if (hashCode == needleCode && haystack.substring(i - needleLen + 1, i + 1).equals(needle))
+                return i - needleLen + 1;
         }
         return -1;
     }
